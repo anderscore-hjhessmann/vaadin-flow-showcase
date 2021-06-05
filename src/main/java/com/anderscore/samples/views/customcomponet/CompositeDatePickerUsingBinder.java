dff428ff5d93a0a771e5d8df5ed73a3a93edf30f
anderscore-hjhessmann <hans.joerg.hessmann@anderscore.com>
2021-06-05 19:37:01 +0200

    Einfach Validierung

--- src/main/java/com/anderscore/samples/views/customcomponet/CompositeDatePickerUsingBinder.java
+++ src/main/java/com/anderscore/samples/views/customcomponet/CompositeDatePickerUsingBinder.java
@@ -3,14 +3,15 @@ package com.anderscore.samples.views.customcomponet;
 import com.vaadin.flow.component.AbstractCompositeField;
 import com.vaadin.flow.component.orderedlayout.HorizontalLayout;
 import com.vaadin.flow.component.textfield.IntegerField;
-import com.vaadin.flow.data.binder.BeanValidationBinder;
-import com.vaadin.flow.data.binder.ValidationException;
+import com.vaadin.flow.data.binder.*;
+import com.vaadin.flow.data.validator.AbstractValidator;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import javax.validation.constraints.Max;
 import javax.validation.constraints.Min;
 import java.lang.invoke.MethodHandles;
+import java.time.DateTimeException;
 import java.time.LocalDate;
 
 public class CompositeDatePickerUsingBinder extends AbstractCompositeField<HorizontalLayout, CompositeDatePickerUsingBinder, LocalDate> {
@@ -32,16 +33,27 @@ public class CompositeDatePickerUsingBinder extends AbstractCompositeField<Horiz
         month.addValueChangeListener(this::update);
         year.addValueChangeListener(this::update);
         getContent().add(dayOfMonth, month, year);
-        binder.bindInstanceFields(this);
+        binder.withValidator(createValidator()).bindInstanceFields(this);
+    }
+
+    private Validator<LocalDateModel> createValidator() {
+        return new AbstractValidator<LocalDateModel>("Kein gültiges Datum") {
+            @Override
+            public ValidationResult apply(LocalDateModel value, ValueContext context) {
+                try {
+                    value.getDate();
+                } catch (DateTimeException ex) {
+                    log.info("Validation failed: {}", ex.getMessage());
+                    return ValidationResult.error(ex.getMessage());
+                }
+                return ValidationResult.ok();
+            }
+        };
     }
 
     private void update(ValueChangeEvent<Integer> ev) {
         LocalDateModel model = new LocalDateModel();
-        try {
-            binder.writeBean(model);
-        } catch (ValidationException ex) {
-            log.error("Validation failed: " + ex, ex);
-        }
+        binder.writeBeanIfValid(model);
         setModelValue(model.getDate(), true);
     }
 
@@ -50,6 +62,30 @@ public class CompositeDatePickerUsingBinder extends AbstractCompositeField<Horiz
         LocalDateModel model = new LocalDateModel();
         model.setDate(date);
         binder.readBean(model);
+    }
+
+    @Override
+    public void setRequiredIndicatorVisible(boolean requiredIndicatorVisible) {
+        dayOfMonth.setRequiredIndicatorVisible(requiredIndicatorVisible);
+        month.setRequiredIndicatorVisible(requiredIndicatorVisible);
+        year.setRequiredIndicatorVisible(requiredIndicatorVisible);
+    }
+
+    @Override
+    public boolean isRequiredIndicatorVisible() {
+        return year.isRequiredIndicatorVisible();
+    }
+
+    @Override
+    public void setReadOnly(boolean readOnly) {
+        dayOfMonth.setReadOnly(readOnly);
+        month.setReadOnly(readOnly);
+        year.setReadOnly(readOnly);
+    }
+
+    @Override
+    public boolean isReadOnly() {
+        return year.isReadOnly();
     }
 
     public static class LocalDateModel {
